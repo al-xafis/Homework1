2eb21b24f029f4abaca1ff6cbd8a3890d1d849de
Al-Khafis Muzrafkulov <al-khafis@MBP.local>
2022-03-21 13:19:35 +0500

    hw4/testing

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const passport = require("passport");
-const mongoose = require("mongoose");
-const countriesRouter = require("./routes/countries");
-const authRouter = require("./routes/auth");
+const { app } = require("./src/app");
 
-const port = process.env.PORT;
-
-mongoose.connect(process.env.MONGOURI, () => {
-  console.log("connected to DB");
-});
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use(passport.initialize());
-
-app.use("/", authRouter);
-app.use("/countries", countriesRouter);
-
-app.listen(port, () => {
-  console.log("Server is running at port: " + port);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running at port: " + process.env.PORT);
 });
