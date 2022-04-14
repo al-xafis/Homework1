c916b41032556d73d5e6afbb7e04b73fe335e9dc
al-khafis <alkh4fis@gmail.com>
2022-04-14 08:48:06 +0500

    backend transaction

--- server/src/routes/transaction.js
+++ server/src/routes/transaction.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Decimal = require("decimal.js");
 
 const { auth } = require("../passport");
 const User = require("../models/users");
@@ -29,6 +30,12 @@ router.post("/", async (req, res) => {
       payee,
       date,
     });
+    if (type === "Expense") {
+      account.amount = new Decimal(account.amount).minus(amount).toNumber();
+    } else if (type === "Income") {
+      account.amount = new Decimal(account.amount).plus(amount).toNumber();
+    }
+
     await user.save();
     res.status(201).json(user);
   } catch (err) {
@@ -48,6 +55,70 @@ router.get("/", async (req, res) => {
     );
 
     res.status(201).json(account.transactions);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const email = req.query.email;
+  const accountTitle = req.query.accountTitle;
+  const id = req.params.id;
+
+  const amount = req.body.amount;
+
+  try {
+    const user = await User.findOne({ email });
+    const account = user.accounts.find(
+      (account) => account.title === accountTitle
+    );
+    let transaction = account.transactions.find((transaction) => {
+      if (transaction.type === "Expense") {
+        account.amount += transaction.amount;
+        account.amount -= amount;
+      } else if (transaction.type === "Income") {
+        account.amount -= transaction.amount;
+        account.amount += amount;
+      }
+      return transaction._id.equals(id);
+    });
+    console.log(transaction);
+
+    transaction.amount = amount;
+
+    user.save();
+
+    res.status(201).json("successfully edited");
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const email = req.query.email;
+  const accountTitle = req.query.accountTitle;
+  const id = req.params.id;
+  console.log(id);
+
+  try {
+    const user = await User.findOne({ email });
+    const account = user.accounts.find(
+      (account) => account.title === accountTitle
+    );
+
+    account.transactions = account.transactions.filter((transaction) => {
+      if (transaction.type === "Expense") {
+        account.amount -= transaction.amount;
+      } else if (transaction.type === "Income") {
+        account.amount += transaction.amount;
+      }
+      return !transaction._id.equals(id);
+    });
+    console.log(account.transactions);
+    user.save();
+
+    res.status(201).json("successfully deleted");
   } catch (err) {
     res.status(400).json(err);
   }
